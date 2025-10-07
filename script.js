6893fb651ba2a95a37491ac3bf9e04324e83a32f
w1257 <wiktorarnold.kaminski@gmail.com>
2025-10-07 14:52:49 +0100

    started adding blogs. non-working.

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-async function loadPage(page){
+async function loadPage(basepath,page){
     const container = document.getElementById('pageToLoad');
+    const baseContainer = document.getElementById('basePagerContainer');
+    baseContainer.href = basepath;
     if (container) {
         const response = await fetch(page);
         container.innerHTML = await response.text();;
     }
 }
 
-document.addEventListener('DOMContentLoaded',() => {loadPage("./aboutMe.html")})
+document.addEventListener('DOMContentLoaded',() => {loadPage("./pages/","./aboutMe.html")})
