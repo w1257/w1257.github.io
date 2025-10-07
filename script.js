053573227914f60259011846ab019dde0f4a65e0
w1257 <wiktorarnold.kaminski@gmail.com>
2025-10-07 15:39:42 +0100

    Blog pages now load properly

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 async function loadPage(basepath,page){
     const container = document.getElementById('pageToLoad');
     const baseContainer = document.getElementById('basePagerContainer');
-    baseContainer.href = basepath;
-    console.log("../"+basepath+page);
-    
+    baseContainer.href = "";
     if (container) {
-        const response = await fetch("../"+basepath+page);
+        const response = await fetch(basepath+page);
         container.innerHTML = await response.text();;
     }
+    baseContainer.href = basepath;
 }
 
 document.addEventListener('DOMContentLoaded',() => {loadPage("./pages/","aboutMe.html")})
