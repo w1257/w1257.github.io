29e032847349d2703af17d2627146dea2f85132c
w1257 <wiktorarnold.kaminski@gmail.com>
2025-10-07 15:07:07 +0100

    Still not working, added debug stuff

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ async function loadPage(basepath,page){
     const container = document.getElementById('pageToLoad');
     const baseContainer = document.getElementById('basePagerContainer');
     baseContainer.href = basepath;
+    console.log("../"+basepath+page);
+    
     if (container) {
-        const response = await fetch(page);
+        const response = await fetch("../"+basepath+page);
         container.innerHTML = await response.text();;
     }
 }
 
-document.addEventListener('DOMContentLoaded',() => {loadPage("./pages/","./aboutMe.html")})
+document.addEventListener('DOMContentLoaded',() => {loadPage("./pages/","aboutMe.html")})
